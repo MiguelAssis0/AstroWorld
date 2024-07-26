e3a098c53bd000d67425a8f8881af36468009165
Miguel Kawe dos Anjos Assis <miganjos88@gmail.com>
2024-07-26 11:29:20 -0300

    teste

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
                     <li><a href="/">Home</a></li>
                     <li><a href="/about">Sobre</a></li>
                     <li><a href="/contact">Contato</a></li>
-                </ul>
+                </ul> 
             </nav>
             <div className="register">
                 <a href="/login">Login</a>
